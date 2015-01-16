366528fb24e03256e51dc3f437f7b08658619a95
Gerhard Burger <gerhard@svi.nl>
2015-01-16 11:03:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/gerhard/hamcrestmatcherswithproguard/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.gerhard.hamcrestmatcherswithproguard;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.hamcrest.Matcher;
+
+import static org.hamcrest.Matchers.greaterThan;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = "MainActivity";
+
+    public static <T> boolean checkThat(T actual, Matcher<? super T> matcher) {
+        return matcher.matches(actual);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "check: " + checkThat(3.0, greaterThan(2.0)));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
